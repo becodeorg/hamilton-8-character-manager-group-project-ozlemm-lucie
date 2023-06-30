8bcbf05cd55087c3c21efa7ed8c4304f7a80a526
[Lucie Monteiro] <[lucie.mtro@gmail.com]>
2023-06-30 09:52:15 +0200

    finition de card menu

--- js/home.js
+++ js/home.js
@@ -1,30 +1,32 @@
 const container_cards = document.querySelector(".container");
 
+
 const cards = fetch("https://character-database.becode.xyz/characters")
     .then(response => response.json())
-    .then((cards =>{
+    .then((cards => {
         for (hero of cards){
             div_card = document.createElement("div");
             container_cards.appendChild(div_card);
             image = document.createElement("img");
             nameHero = document.createElement("div");
             description = document.createElement("p");
-            see = document.createElement("a");
-            see.innerText = 'See Character';
             div_card.appendChild(image);
             div_card.appendChild(nameHero);
             div_card.appendChild(description);
-            div_card.appendChild(see);
             div_card.classList.add('card');
             image.classList.add("image");
             nameHero.classList.add("name_hero");
-            nameHero.innerText = hero.name;
             description.classList.add("description");
+            nameHero.innerText = hero.name;
             description.innerText = hero.description;
+            see = document.createElement("a");
+            see.innerText = 'See Character';
             see.classList.add("see");
-            console.log(cards);
+            div_card.appendChild(see)
+            see.href = "../pages/single.html?name=" + hero['id'];
         }
     }))
+
 
 
 
