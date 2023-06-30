a556e9acf140915123eb8ddb3bcfa9ec1a114f98
[Lucie Monteiro] <[lucie.mtro@gmail.com]>
2023-06-30 15:19:06 +0200

    creation faite

--- js/creation.js
+++ js/creation.js
@@ -1,37 +1,51 @@
 const card = document.querySelector(".card_single_creation");
 const image = document.querySelector(".image_creation");
 const uploadImage = document.querySelector(".upload_image");
-const nameHero = document.querySelector(".input_name_hero");
-const shortDescription = document.querySelector(".input_short_description");
-const description = document.querySelector(".input_description");
 const textUser = document.querySelector(".text_user");
 const buttonDelete = document.querySelector(".delete_character");
-const buttonSave = document.querySelector(".save_changes");
+let Api = "https://character-database.becode.xyz/";
 
 
 
-
-
-
-
-
-
-buttonDelete.addEventListener("click", () => {
-    fetch("https://character-database.becode.xyz/characters/" + postName, {
-        method: "DELETE",
-    })
-    .then(response => {
-        if (response.ok){
-            console.log("bien supprimé");
+document.querySelector('#save_changes').onclick=function(e)
+{
+    e.preventDefault();
+    let block = false;
+    let inputs =document.querySelectorAll('input');
+    for (input of inputs)
+    {
+        if(input.value =="")
+        {
+         input.style.background = 'red';
+         block = true;
         }
-        else{
-            console.log("pas supprimé error");
-        }
-    })
-    .catch(error => {
-        console.error("erreur :", error);
-    });
-});
+    }
+
+    if(block == false)
+    {
+        const nameHero = document.querySelector(".input_name_hero").value;
+        const shortDescription = document.querySelector(".input_short_description").value;
+        const description = document.querySelector(".input_description").value;
+
+        (async () => {
+            const rawResponse = await fetch(Api+ "characters", {
+              method: 'POST',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body:JSON.stringify({
+                name: nameHero,
+                shortDescription:shortDescription,
+                description : description
+            }) 
+            });
+          })();
+
+    }
+}
+
+
 
 
 
