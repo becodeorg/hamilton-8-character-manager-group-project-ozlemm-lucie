c83f241c7ee43b2314c3bf2ac0ad22ee719ae8e9
Ozlem Kandemir <130037168+OzlemKan@users.noreply.github.com>
2023-06-30 15:52:37 +0200

    editor

--- js/creation.js
+++ js/creation.js
@@ -5,48 +5,41 @@ const textUser = document.querySelector(".text_user");
 const buttonDelete = document.querySelector(".delete_character");
 let Api = "https://character-database.becode.xyz/";
 
-
-
-document.querySelector('#save_changes').onclick=function(e)
-{
-    e.preventDefault();
-    let block = false;
-    let inputs =document.querySelectorAll('input');
-    for (input of inputs)
-    {
-        if(input.value =="")
-        {
-         input.style.background = 'red';
-         block = true;
-        }
+document.querySelector("#save_changes").onclick = function (e) {
+  e.preventDefault();
+  let block = false;
+  let inputs = document.querySelectorAll("input");
+  for (input of inputs) {
+    if (input.value == "") {
+      input.style.background = "red";
+      block = true;
     }
+  }
 
-    if(block == false)
-    {
-        const nameHero = document.querySelector(".input_name_hero").value;
-        const shortDescription = document.querySelector(".input_short_description").value;
-        const description = document.querySelector(".input_description").value;
+  if (block == false) {
+    const nameHero = document.querySelector(".input_name_hero").value;
+    const shortDescription = document.querySelector(
+      ".input_short_description"
+    ).value;
+    const description = document.querySelector(".input_description").value;
 
-        (async () => {
-            const rawResponse = await fetch(Api+ "characters", {
-              method: 'POST',
-              headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              },
-              body:JSON.stringify({
-                name: nameHero,
-                shortDescription:shortDescription,
-                description : description
-            }) 
-            });
-          })();
+    (async () => {
+      const rawResponse = await fetch(Api + "characters", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: nameHero,
+          shortDescription: shortDescription,
+          description: description,
+        }),
+      });
+    })();
+  }
+};
 
-    }
-}
-
-
-
-
-
-  
+tinymce.init({
+  selector: "#myEditor",
+});
