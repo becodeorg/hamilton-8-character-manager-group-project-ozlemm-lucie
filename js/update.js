4db564f05206fa96b3dca3c8bc875896d67567e6
[Lucie Monteiro] <[lucie.mtro@gmail.com]>
2023-07-03 15:53:08 +0200

    recherche

--- js/update.js
+++ js/update.js
@@ -37,28 +37,21 @@ const update = fetch("https://character-database.becode.xyz/characters")
 const card = document.querySelector(".card_update");
 let inputImage;
 
-//Variable qui va chercher input type = file
 const fileSelector = document.querySelector('.uploadImage');
-//On écoute l'évènement change de fileSelector
 fileSelector.addEventListener('change', (event) => {
-  //Variable qui permet d'accéder aux fichiers capté par input type = file
   const fileList = event.target.files;
-  //Log les metadata des fichiers
   console.log(fileList);
 
   readImage(fileList[0]);
 });
 
 function readImage(file) {
-  // Vérifie que le fichier est une image.
   if (file.type && !file.type.startsWith('image/')) {
     console.log('File is not an image.', file.type, file);
     return;
   }
 
   const reader = new FileReader();
-
-  //On écoute n'évènement load de reader
   reader.addEventListener('load', (event) => {
     inputImage = event.target.result.split(',')[1];;
   });
@@ -66,19 +59,12 @@ function readImage(file) {
   reader.readAsDataURL(file);
 }
 
-//Configurer le bouton permettant l'envoi des infos vers l'API
-
-
-
-//Configurer le bouton pour faire que lorsqu'on clique dessus, cela envoi les données vers l'API
 buttonEdit.addEventListener("click", function (event) {
-  //Empêche le comportement par défaut du bouton, à savoir, recharger la page
+
   event.preventDefault();
   let inputName = nameHeroUpdate.value;
   let inputShortDescription = shortDescription.value;
   let inputDescription = description.value;
-
-  //Construire l'objet avec les valeurs du formulaire, en accord avec le contrat de l'API (défini dans une doc)
 
   const newCharacter = {
     name: inputName,
@@ -86,19 +72,15 @@ buttonEdit.addEventListener("click", function (event) {
     image: inputImage,
     description: inputDescription,
   }
-//Vérification que les étapes précédentes fonctionnent
+
   console.log(newCharacter) //ok
   fetch("https://character-database.becode.xyz/characters/" + postName, {
     method: "PUT",
-    //Le header défini que les données qu'on envoi seront en format JSON (metadata, complément d'information à destination de l'API)
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(newCharacter),
   })
-
-
-
 
     .then(response => response.json())
     .then(result => {
