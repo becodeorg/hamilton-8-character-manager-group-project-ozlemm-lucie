6ef151849ebabfc338c063f4825fbc2a7fe16d83
[Lucie Monteiro] <[lucie.mtro@gmail.com]>
2023-07-03 14:55:17 +0200

    update REUSSI

--- js/single.js
+++ js/single.js
@@ -4,19 +4,24 @@ const shortDescription = document.querySelector(".short_description_single")
 const description = document.querySelector(".description_single");
 const imageSingle = document.querySelector(".image_single");
 const buttonDelete = document.querySelector(".button_delete")
-const buttonUpdate = document.querySelector(".button_update")
+
+
+
 
 
 const single = fetch("https://character-database.becode.xyz/characters")
     .then((id) => id.json())
     .then((id) => {
         for (let elements of id ){
+            const buttonUpdate = document.querySelector(".button_update")
+            buttonUpdate.href = "../pages/update.html?id=" + elements.id;
             if (elements.id === postName){
                 nameHeroSingle.innerText = elements.name
                 shortDescription.innerText = elements.shortDescription
                 description.innerText = elements.description
                 imageSingle.src=  'data:image/gif;base64,' + elements.image;
                 image.classList.add("image_single");
+
             }
         }
     })
--- js/update.js
+++ js/update.js
@@ -1,7 +1,28 @@
-const card = document.querySelector(".card_single_creation");
-const buttonDelete = document.querySelector(".delete_character");
-let api = "https://character-database.becode.xyz/";
+const postName = (new URLSearchParams(window.location.search)).get('id');
+const nameHeroUpdate = document.querySelector(".input_name_hero_update");
+const shortDescription = document.querySelector(".input_short_description_update")
+const description = document.querySelector(".input_description_update");
+const imageUpdate = document.querySelector(".image_update");
+const buttonEdit = document.querySelector(".button_edit")
 
+
+
+const update = fetch("https://character-database.becode.xyz/characters")
+    .then((id) => id.json())
+    .then((id) => {
+        for (let elements of id ){
+            if (elements.id === postName){
+                nameHeroUpdate.value = elements.name
+                shortDescription.value = elements.shortDescription
+                description.value = elements.description
+                imageUpdate.src=  'data:image/gif;base64,' + elements.image;
+                image.classList.add("image_update");
+            }
+        }
+    })
+
+
+const card = document.querySelector(".card_update");
 let inputImage;
 
 //Variable qui va chercher input type = file
@@ -34,24 +55,14 @@ function readImage(file) {
 }
 
 //Configurer le bouton permettant l'envoi des infos vers l'API
-//Variable pour cibler le bouton
-const creationButton = document.querySelector("#save_changes");
+
+
 
 //Configurer le bouton pour faire que lorsqu'on clique dessus, cela envoi les données vers l'API
-creationButton.addEventListener("click", function (event) {
+buttonEdit.addEventListener("click", function (event) {
   //Empêche le comportement par défaut du bouton, à savoir, recharger la page
   event.preventDefault();
-  //Déclare l'adresse de l'API
-  const url = "https://character-database.becode.xyz/characters";
-//Déclare des variables pour récupérer chaque élément (en l'occurence: input, sauf textearea)
-
-  let name = document.querySelector(".input_name_hero");
-  let shortDescription = document.querySelector(".short_description_creation");
-  let description = document.querySelector(".input_description");
-
-  //Déclare des variables récupérant les données entrées par l'utilisateur
-
-  let inputName = name.value;
+  let inputName = nameHeroUpdate.value;
   let inputShortDescription = shortDescription.value;
   let inputDescription = description.value;
 
@@ -61,36 +72,29 @@ creationButton.addEventListener("click", function (event) {
     name: inputName,
     shortDescription: inputShortDescription,
     image: inputImage,
-    description: inputDescription
+    description: inputDescription,
   }
 //Vérification que les étapes précédentes fonctionnent
   console.log(newCharacter) //ok
-  //Fetch va chercher dans l'url définie par la variable
-  fetch(url, {
-    //POST car on va ajouter des données dans l'API, et c'est surtout ainsi que la documentation de l'API demande de le faire, c'est différent selon l'API
-    method: "POST",
+  fetch("https://character-database.becode.xyz/characters/" + postName, {
+    method: "PUT",
     //Le header défini que les données qu'on envoi seront en format JSON (metadata, complément d'information à destination de l'API)
     headers: {
       "Content-Type": "application/json",
     },
-    //Transforme les données envoyées en string format JSON dans le body (qui est le corps de la requête, ce que j'envoi effectivement à l'API)
     body: JSON.stringify(newCharacter),
   })
-    //Traitement de la réponse: 
-    //Then = le bloc précédent a répondu et on défini quoi faire ensuite
-    //response = TOUTE la réponse du serveur, puis on spécifie qu'on ne veut que la partie JSON
+
+
+
+
     .then(response => response.json())
-    //Quand on a reçu les données JSON demandées, on demande de log le message "Success" et la réponse
     .then(result => {
       console.log("Success:", result);
-
-      // Redirection vers une autre page
-      // TODO: la bonne page
       window.location.href = "../index.html";
     })
-    //Catch intercepte une erreur s'il y en a
+ 
     .catch(error => {
-      // Puis log le message "ERROR" et une erreur résultant de mon fetch (404, pas de network, etc.)
       console.error("Error:", error);
     });
 })
